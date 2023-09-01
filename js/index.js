f5c60b9a8c0ddf0958b17fd627b2d8080f639c1c
Tea Sallstedt Elming <ts223ir@student.lnu.se>
2023-09-01 12:08:32 +0200

    Functioning reversal

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// This is a class
+export class Index {
+  constructor() {
+    // Input custom element
+    this.nameInputElement = document.createElement('name-input')
+
+    // Append the name-input element to the nameInputHere div
+    const nameInputContainer = document.getElementById('nameInputHere')
+    nameInputContainer.appendChild(this.nameInputElement)
+
+    // Name entered event from NameInput class
+    document.addEventListener('nameEntered', this.handleNameEntered.bind(this))
+  }
+
+  reverseName(string) {
+    console.log(string)
+    // Turn name string into array
+    const nameInput = string.toLowerCase()
+    const nameArray = nameInput.split('')
+    const nameLength = nameArray.length // get lenght of name
+
+    let backwardsArray = [] // empty array
+
+    for (let i = 0; i < nameLength; i++) {
+      backwardsArray[i] = nameArray[nameLength - (i + 1)] // length is one more than highest index
+    }
+    const reversedName = backwardsArray.join('')
+
+    return reversedName
+  }
+
+  handleNameEntered(event) {
+    const responseContainer = document.getElementById('responseInputHere')
+
+    console.log(this.nameInputElement.nameInput)
+    const submittedName = event.detail.nameInput
+    console.log(submittedName)
+    const reversedName = this.reverseName(submittedName)
+
+    const responseDiv = document.createElement('div')
+    responseDiv.textContent = `Your name in reverse is: ${reversedName}`
+
+    responseContainer.appendChild(responseDiv)
+  }
+}
+
+new Index()
--- js/nameInput.js
+++ js/nameInput.js
@@ -9,25 +9,30 @@ template.innerHTML = `
 `
 
 class NameInput extends HTMLElement {
-  constructor () {
+  constructor() {
     super()
+
+    this.attachShadow({ mode: 'open' })
+      .appendChild(template.content.cloneNode(true))
+
 
     this.nameInput = 'Blank Space'
   }
 
   // When added to the DOM
-  connectedCallback () {
+  connectedCallback() {
     this.form = this.shadowRoot.querySelector('form')
-    this.input = this.shadowRoot.querySelector('"input-name')
+    this.input = this.shadowRoot.querySelector('#input-name')
 
     this.form.addEventListener('submit', (event) => {
       event.preventDefault()
       if (this.input.value !== '') {
         this.nameInput = this.input.value
-        this.remove()
-        const nameEnteredEvent = new CustomEvent('nameEntered')
+        console.log(this.nameInput)
+        const nameEnteredEvent = new CustomEvent('nameEntered', { detail: { nameInput: this.nameInput}})
         document.dispatchEvent(nameEnteredEvent)
       }
     })
   }
 }
+customElements.define('name-input', NameInput)
